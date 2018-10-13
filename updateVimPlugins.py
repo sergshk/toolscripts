96cfd383543c0cf84ddab8e01a639d57f2b337d1
Sergey Shkundaleu <sergey@ntiviti.com>
2018-10-13 12:14:43 -0500

    Commiting usable version of the script

--- updateVimPlugins.py
+++ updateVimPlugins.py
@@ -7,65 +7,39 @@
 
 # First of all importing neccesary modules
 import subprocess 
+import json
 
 # DEFINE constants
 pathToTmp = '/tmp'
 pathToDotVim = 'dotvim'
-# various URLs
-urltaglist = "https://www.vim.org/scripts/download_script.php?src_id=19574"
-urlvcscommand = "https://github.com/vim-scripts/vcscommand.vim.git"
-urlnerdtree = "https://github.com/scrooloose/nerdtree.git"
-urlsyntastic = "https://github.com/vim-syntastic/syntastic.git"
+confRepo = "https://github.com/sergshk/dotfls.git"
+
+# defining method to install plugins from github or direct download of a zip file
+def installPlugin(folderName,method,url,fileName):
+    if ("git" == method):
+        # just cloning repo
+        subprocess.call([method,"clone",url],cwd=pathToTmp)
+    else:
+        # we have to download zip
+        # then extract and delete zip file to keep tmp clean
+        subprocess.call(["mkdir",folderName],cwd=pathToTmp)
+        # download zip into folder we've created above
+        subprocess.call([method,"-O",pathToTmp+ "/" + folderName + "/" + fileName,url])
+        subprocess.call(["unzip",fileName],cwd=pathToTmp+"/"+folderName)
+        # removing craft
+        subprocess.call(["rm","-fR",pathToTmp + "/" + folderName + "/" + fileName])
+    subprocess.call("cp -r "+pathToTmp+"/"+folderName+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
+    subprocess.call(["rm","-fR",pathToTmp + "/" + folderName])
+# cloning my conf repo into tmp
+subprocess.call(["git","clone",confRepo],cwd=pathToTmp)
+# reading JSON file
+with open(pathToTmp+"/dotfls/vim_plugins.json") as myfile:
+    confJson = json.load(myfile)
 
 # TODO Check if folder exist and if not create it.
 subprocess.call(["mkdir",pathToDotVim],cwd=pathToTmp)
+#  move throug JSON file and install all plugins
+for plugin in confJson["plugins"]:
+    installPlugin(plugin['name'],plugin['method'],plugin['url'],plugin['filename'])
 
-# TODO Automate this, but for now simply hardcoding each os the 
-# installed modules
-
-# MODULE taglist
-# this has nto been update for ages https://github.com/vim-scripts/taglist.vim
-# as such best way to find is from https://www.vim.org/scripts/script.php?script_id=273
-# my taglist onthe latest (4.6) version
-# TODO check if taglist folder exist
-taglistFolder = "taglist"
-taglistFile = "taglist_46.zip"
-subprocess.call(["mkdir",taglistFolder],cwd=pathToTmp)
-subprocess.call(["wget","-O",pathToTmp+ "/" + taglistFolder + "/" + taglistFile,urltaglist])
-subprocess.call(["unzip",taglistFile],cwd=pathToTmp+"/"+taglistFolder)
-# deleting zip file to keep vimfolder clean
-subprocess.call(["rm","-fR",pathToTmp + "/" + taglistFolder + "/" + taglistFile])
-# copy plugin into vimfolder
-subprocess.call("cp -r "+pathToTmp+"/"+taglistFolder+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
-# cleanup
-subprocess.call(["rm","-fR",pathToTmp + "/" + taglistFolder])
-
-# MODULE vcscommand
-# using GitHub repository git clone https://github.com/vim-scripts/vcscommand.vim.git
-# clone repo
-vcscommandFolder = "vcscommand.vim"
-subprocess.call(["git","clone",urlvcscommand],cwd=pathToTmp)
-# move files into my repository 
-subprocess.call("cp -r "+pathToTmp+"/"+vcscommandFolder+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
-# cleanup
-subprocess.call(["rm","-fR",pathToTmp + "/" + vcscommandFolder])
-
-# MODULE NERDTree
-# using GitHub repository git clone https://github.com/scrooloose/nerdtree.git
-# clone repo
-subprocess.call(["git","clone",urlnerdtree],cwd=pathToTmp)
-# move files into my repository
-nerdtreeFolder = "nerdtree"
-subprocess.call("cp -r "+pathToTmp+"/"+nerdtreeFolder+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
-# cleanup
-subprocess.call(["rm","-fR",pathToTmp + "/" + nerdtreeFolder])
-
-# MODULE sysntastic 
-# clone repo
-subprocess.call(["git","clone",urlsyntastic],cwd=pathToTmp)
-# move files into my repository
-syntasticFolder = "syntastic"
-subprocess.call("cp -r "+pathToTmp+"/"+syntasticFolder+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
-# cleanup
-subprocess.call(["rm","-fR",pathToTmp + "/" + syntasticFolder])
-
+# TODO add copy of the files and backup of old files
