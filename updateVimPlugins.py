df96d46ec2d9994b9330e95ffc3e39f0e2f2ed4f
Sergey Shkundaleu <sergey@iontec.org>
2020-01-09 12:28:40 -0600

    Updating script with some extra things

--- updateVimPlugins.py
+++ updateVimPlugins.py
@@ -9,19 +9,46 @@
 import subprocess 
 import json
 import sys
+import os
 
 # DEFINE constants
 pathToTmp = '/tmp'
 confRepo = "https://github.com/sergshk/dotfls.git"
-if (len(sys.argv) > 1 and "--essential" == sys.argv[1]):
-    pathToDotVim = 'dotvimessential'
-    pathToConfJson = '../dotfls/vim_essential.json'
-else:
-    pathToDotVim = 'dotvim'
-    pathToConfJson = 'vim_plugins.json'
+helpText = """Command was executed without any parameters, potential mistake?
+    Try using --dry-run or --commit to view and commit changes """
+usageText = """Usage: updateVimPlugins.py [ARGS]
+    -h, --help      display help text
+    -e, --esential  install essential version
+    -c, --commit    commit changes
+    -v, --verbose   verbose output
+    -d, --dry-run   display potential changes"""
+scriptName=sys.argv[0]
+allArgs = sys.argv[1:]
+if len(allArgs) < 1:
+    print(helpText)
+    print(usageText)
+    sys.exit(0)
+
+# By default running full vim install
+pathToDotVim = 'dotvim'
+pathToConfJson = 'vim_plugins.json'
+verboseOut = False
+
+# process arguments
+for oneArg in allArgs:
+    if oneArg in ("-h","--help"):
+        print(usageText)
+    elif oneArg in ("-e","--essential"):
+        pathToDotVim = 'dotvimessential'
+        pathToConfJson = 'vim_essential.json'
+    elif oneArg in ("-d","--dry-run"):
+        dryRunFlag = True
+    elif oneArg in ("-v","--verbose"):
+        verboseOut = True
 
 # defining method to install plugins from github or direct download of a zip file
 def installPlugin(folderName,method,url,fileName):
+    printOutput("Installing <" + folderName + "> pugin using method <" + method + ":" + url + ">")
     if ("git" == method):
         # just cloning repo
         subprocess.call([method,"clone",url],cwd=pathToTmp)
@@ -36,14 +63,29 @@ def installPlugin(folderName,method,url,fileName):
         subprocess.call(["rm","-fR",pathToTmp + "/" + folderName + "/" + fileName])
     subprocess.call("cp -r "+pathToTmp+"/"+folderName+"/* "+pathToTmp+"/"+pathToDotVim+"/",shell=True)
     subprocess.call(["rm","-fR",pathToTmp + "/" + folderName])
+# defining method to print output
+def printOutput(outputStr):
+    if verboseOut:
+        print("+++++:" + outputStr)
+
 # cloning my conf repo into tmp
+printOutput("Clonning repository " + confRepo)
+#sys.exit(0)
 subprocess.call(["git","clone",confRepo],cwd=pathToTmp)
 # reading JSON file
 with open(pathToTmp+"/dotfls/"+pathToConfJson) as myfile:
     confJson = json.load(myfile)
 
-# TODO Check if folder exist and if not create it.
-subprocess.call(["mkdir",pathToDotVim],cwd=pathToTmp)
+# Check if folder exist and if not create it or delete content.
+if os.path.isdir(pathToTmp + "/" + pathToDotVim):
+    printOutput("Clearing folder " + pathToTmp + "/" + pathToDotVim)
+    subprocess.call(["rm","-fR",pathToTmp + "/" + pathToDotVim + "/*"])
+elif os.path.exists(pathToTmp + pathToDotVim):
+    printOutput("Removing file " + pathToTmp + "/" + pathToDotVim)
+    os.remove(pathToTmp + "/" + pathToDotVim)
+else:
+    printOutput("Creating folder " + pathToTmp + "/" + pathToDotVim)
+    subprocess.call(["mkdir",pathToDotVim],cwd=pathToTmp)
 #  move throug JSON file and install all plugins
 for plugin in confJson["plugins"]:
     installPlugin(plugin['name'],plugin['method'],plugin['url'],plugin['filename'])
